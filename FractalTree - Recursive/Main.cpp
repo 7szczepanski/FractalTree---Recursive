0a9031a15697de5a8b2a6db8bed6fb738f6b6a2a
7szczepanski <7szczepanski@gmail.com>
2017-03-19 15:31:35 +0100

    it need some fixes

--- FractalTree - Recursive/Main.cpp
+++ FractalTree - Recursive/Main.cpp
@@ -1,10 +1,78 @@
 #include <SFML/Graphics.hpp>
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+using namespace sf;
+
+int width = 800;
+int height = 800;
+class Line {
+public:
+	Line(Vector2f a_, Vector2f b_) {
+		a = a_;
+		b = b_;
+	}
+	
+	void show(RenderWindow &target_) {
+		sf::Vertex line[] =
+		{
+			sf::Vertex(sf::Vector2f(a.x, a.y)),
+			sf::Vertex(sf::Vector2f(b.x, b.y))
+		};
+		
+		target_.draw(line, 2, sf::Lines);
+	}
+	
+protected:
+	Vector2f a;
+	Vector2f b;
+};
+
+
+void branch(vector<Line> &lines, Vector2f start_, int len_) {
+	float angleL = -25;
+	float angleR = 25;
+	float len = len_;
+	if (len == 3) return;
+		Vector2f endL;
+		Vector2f endR;
+		Vector2f start = start_;
+		endL = Vector2f(start.x, start.y - len);
+		Transform rotL;
+		rotL.rotate(angleL, start.x, start.y);
+		Vector2f positionARL = rotL.transformPoint(endL);
+		endL = positionARL;
+
+		endR = Vector2f(start.x, start.y - len);
+		Transform rotR;
+		rotR.rotate(angleR, start.x, start.y);
+		Vector2f positionARR = rotR.transformPoint(endR);
+		endR = positionARR;
+		
+		Line left(start, endL);
+		Line right(start, endR);
+
+		lines.push_back(right);
+		lines.push_back(left);
+		if (len > 3) {
+			branch(lines, endL, len * 0.67);
+			branch(lines, endR, len*0.67);
+		}
+}
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(width, height), "Fractal Tree - Recursive");
+	sf::RectangleShape background(Vector2f(width, height));
+	background.setFillColor(Color(51, 51, 51));
+
+	vector<Line> lines;
+	Vector2f st(Vector2f(width / 2, height-150));
+	branch(lines,st, 150);
+	int len = 250;
 
 	while (window.isOpen())
 	{
@@ -16,7 +84,15 @@ int main()
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(background);
+		/////////////////////////
+
+		for (int i = 0; i < lines.size(); i++) {
+			lines[i].show(window); //cout << "Bramch painted -------------------------------------\n";
+		}
+	
+		//branch(len);
+		
 		window.display();
 	}
 
