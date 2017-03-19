4ec29a9506c4fb8e229316b23230383cecf7deef
7szczepanski <7szczepanski@gmail.com>
2017-03-19 15:46:25 +0100

    Finaly fixed, kind of

--- FractalTree - Recursive/Main.cpp
+++ FractalTree - Recursive/Main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace sf;
 
-int width = 800;
+int width = 1200;
 int height = 800;
 class Line {
 public:
@@ -14,52 +14,52 @@ public:
 		a = a_;
 		b = b_;
 	}
-	
+
 	void show(RenderWindow &target_) {
 		sf::Vertex line[] =
 		{
 			sf::Vertex(sf::Vector2f(a.x, a.y)),
 			sf::Vertex(sf::Vector2f(b.x, b.y))
 		};
-		
+
 		target_.draw(line, 2, sf::Lines);
 	}
-	
+
 protected:
 	Vector2f a;
 	Vector2f b;
 };
 
 
-void branch(vector<Line> &lines, Vector2f start_, int len_) {
-	float angleL = -25;
-	float angleR = 25;
+void branch(vector<Line> &lines, Vector2f start_, int len_, float aL_, float aR_) {
+	float angleL = aL_;
+	float angleR = aR_;
 	float len = len_;
 	if (len == 3) return;
-		Vector2f endL;
-		Vector2f endR;
-		Vector2f start = start_;
-		endL = Vector2f(start.x, start.y - len);
-		Transform rotL;
-		rotL.rotate(angleL, start.x, start.y);
-		Vector2f positionARL = rotL.transformPoint(endL);
-		endL = positionARL;
+	Vector2f endL;
+	Vector2f endR;
+	Vector2f start = start_;
+	endL = Vector2f(start.x, start.y - len);
+	Transform rotL;
+	rotL.rotate(angleL, start.x, start.y);
+	Vector2f positionARL = rotL.transformPoint(endL);
+	endL = positionARL;
 
-		endR = Vector2f(start.x, start.y - len);
-		Transform rotR;
-		rotR.rotate(angleR, start.x, start.y);
-		Vector2f positionARR = rotR.transformPoint(endR);
-		endR = positionARR;
-		
-		Line left(start, endL);
-		Line right(start, endR);
+	endR = Vector2f(start.x, start.y - len);
+	Transform rotR;
+	rotR.rotate(angleR, start.x, start.y);
+	Vector2f positionARR = rotR.transformPoint(endR);
+	endR = positionARR;
 
-		lines.push_back(right);
-		lines.push_back(left);
-		if (len > 3) {
-			branch(lines, endL, len * 0.67);
-			branch(lines, endR, len*0.67);
-		}
+	Line left(start, endL);
+	Line right(start, endR);
+
+	lines.push_back(right);
+	lines.push_back(left);
+	if (len > 3) {
+		branch(lines, endL, len * 0.67, angleL + 10, -10 - angleR);
+		branch(lines, endR, len*0.67, angleL - 10, -10 + angleR);
+	}
 }
 
 
@@ -70,9 +70,8 @@ int main()
 	background.setFillColor(Color(51, 51, 51));
 
 	vector<Line> lines;
-	Vector2f st(Vector2f(width / 2, height-150));
-	branch(lines,st, 150);
-	int len = 250;
+	Vector2f st(Vector2f(width / 2-220, height));
+	branch(lines, st, 250, 45, 45);
 
 	while (window.isOpen())
 	{
@@ -83,18 +82,29 @@ int main()
 				window.close();
 		}
 
-		window.clear();
-		window.draw(background);
-		/////////////////////////
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
 
-		for (int i = 0; i < lines.size(); i++) {
-			lines[i].show(window); //cout << "Bramch painted -------------------------------------\n";
+			window.clear();
+			window.draw(background);
+			/////////////////////////
+
+			for (int i = 0; i < lines.size(); i++) {
+				lines[i].show(window); //cout << "Bramch painted -------------------------------------\n";
+			}
+
+			//branch(len);
+
+			window.display();
 		}
-	
-		//branch(len);
-		
-		window.display();
+
+		return 0;
 	}
 
-	return 0;
 }
